4b26a3cdd967b02d1041eae341bbe058e44394f3
WHITEPAEK <atomsjp@gmail.com>
2020-09-22 20:10:47 +0900

    인증된 사용자 정보 참조 기능 구현

---
+++ src/main/java/com/tistory/whitepaek/main/MainController.java
@@ -0,0 +1,21 @@
+package com.tistory.whitepaek.main;
+
+import com.tistory.whitepaek.account.CurrentUser;
+import com.tistory.whitepaek.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+
+}
