03e759e7de19a22b4b671fccef81fa3d70603884
WHITEPAEK <atomsjp@gmail.com>
2020-09-28 20:18:31 +0900

    비밀번호가 없는 경우, 이메일로 로그인이 가능한 기능 구현

--- src/main/java/com/tistory/whitepaek/account/AccountService.java
+++ src/main/java/com/tistory/whitepaek/account/AccountService.java
@@ -108,4 +108,13 @@ public class AccountService implements UserDetailsService {
         login(account);
     }
 
+    public void sendLoginLink(Account account) {
+        account.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(account.getEmail());
+        mailMessage.setSubject("로그인 링크");
+        mailMessage.setText("/login-by-email?token=" + account.getEmailCheckToken() + "&email=" + account.getEmail());
+        javaMailSender.send(mailMessage);
+    }
+
 }
